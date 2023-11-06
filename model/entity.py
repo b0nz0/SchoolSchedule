07ef705fa2657841583271a80a5504974ba46432
Federico Di Carlo <fdicarlo@gmail.com>
2023-11-06 14:07:25 +0100

    first version with historic tables

--- model/entity.py
+++ model/entity.py
@@ -59,3 +59,107 @@ class School(Base):
 
     
         
+class SchoolYear(Base):
+
+    _identifier = None
+    _school = None
+
+    def __init__(self) -> None:
+        super(SchoolYear, self).__init__()
+
+    @property
+    def identifier(self):
+        return self._identifier
+    
+    @identifier.setter
+    def identifier(self, identifier):
+        self._identifier = identifier
+
+    @property
+    def school(self):
+        return self._school
+    
+    @school.setter
+    def school(self, school:School):
+        self._school = school
+
+class Year(Base):
+
+    _identifier = None
+    _school = None
+
+    def __init__(self) -> None:
+        super(Year, self).__init__()
+
+    @property
+    def identifier(self):
+        return self._identifier
+    
+    @identifier.setter
+    def identifier(self, identifier):
+        self._identifier = identifier
+
+    @property
+    def school(self):
+        return self._school
+    
+    @school.setter
+    def school(self, school:School):
+        self._school = school
+
+class Section(Base):
+
+    _identifier = None
+    _school = None
+
+    def __init__(self) -> None:
+        super(Section, self).__init__()
+
+    @property
+    def identifier(self):
+        return self._identifier
+    
+    @identifier.setter
+    def identifier(self, identifier):
+        self._identifier = identifier
+
+    @property
+    def school(self):
+        return self._school
+    
+    @school.setter
+    def school(self, school:School):
+        self._school = school
+
+class Class(Base):
+    _schoolYear = None
+    _year = None
+    _section = None
+
+    def __init__(self) -> None:
+        super(Class, self).__init__()
+
+    @property
+    def schoolYear(self):
+        return self._schoolYear
+    
+    @schoolYear.setter
+    def schoolYear(self, schoolYear:SchoolYear):
+        self._schoolYear = schoolYear
+
+    @property
+    def year(self):
+        return self._year
+    
+    @year.setter
+    def year(self, year:Year):
+        self._year = year
+
+    @property
+    def section(self):
+        return self._section
+    
+    @section.setter
+    def section(self, section:Section):
+        self._section = section
+
